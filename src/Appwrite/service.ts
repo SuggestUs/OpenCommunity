bb918898da3d0f2ff32e93181dd85742f40998ba
unknown <hrppanchal27@gmail.com>
2023-05-31 12:58:45 +0530

    Updated folder structure

--- src/Appwrite/service.ts
+++ src/Appwrite/service.ts
@@ -1,12 +1,12 @@
-import { Client, Account } from 'appwrite'
 // import { Client as Appwrite, Databases, Account } from 'appwrite'
+import { Client, Account } from 'appwrite'
 
-const client = new Client()
+const client = new Client();
 client
   .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
   .setProject('6475e3ee034aec0ee319')
 
-const account = new Account(client)
+const account = new Account(client);
 
 // Go to OAuth provider login page
 const loginWithGithub = async () => {
@@ -15,7 +15,7 @@ const loginWithGithub = async () => {
     'http://localhost:5174/home',
     'http://localhost:5174/login'
   )
-  console.log(res)
+  console.log(res);
 }
 
-export default loginWithGithub
+export default loginWithGithub;
