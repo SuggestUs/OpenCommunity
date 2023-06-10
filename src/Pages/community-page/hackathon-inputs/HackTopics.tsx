bddf71267b5f818426bffac96b178333b8bff63d
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2023-06-11 00:32:01 +0530

    feat: added hackathon topics inputs

--- src/Pages/community-page/hackathon-inputs/HackTopics.tsx
+++ src/Pages/community-page/hackathon-inputs/HackTopics.tsx
@@ -1,34 +1,13 @@
 function HackTopicsCom() {
   const posts = [
     {
-      id: 0,
-      title: "Full-Stack Development",
-      href: "#",
-      description:
-        "You have to build a full-stack application using any technology you prefer. You can use any framework or library for frontend",
-      date: "June 9, 2023",
-
-      category: { title: "Web Development" },
+      id: 1,
     },
     {
-      id: 1,
-      title: "Machine Learning",
-      href: "#",
-      description:
-        "You have to build a ML model using any technology you prefer. To make it more interesting, you can use any framework or library",
-      date: "June 9, 2023",
-
-      category: { title: "Machine Learning" },
+      id: 2,
     },
     {
-      id: 1,
-      title: "Web3 Development",
-      href: "#",
-      description:
-        "Build a Web3 web app like a Dapp, NFT marketplace, or anything you want. You can use any framework or library for frontend",
-      date: "June 9, 2023",
-
-      category: { title: "Web3" },
+      id: 3,
     },
   ];
   return (
@@ -40,23 +19,25 @@ function HackTopicsCom() {
             className="flex max-w-xl border p-5 hover:border-gray-600 rounded-md flex-col items-start justify-between"
           >
             <div className="flex items-center gap-x-4 text-xs">
-              <time dateTime={post.date} className="text-gray-500">
-                {post.date}
-              </time>
-              <p className="relative z-10 rounded-full bg-gray-300 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">
-                {post.category.title}
-              </p>
+              <input type="date" className="text-gray-500 outline-none" />
+              <input
+                type="text"
+                placeholder="web development"
+                className="relative z-10 rounded-full w-1/2 outline-none  bg-gray-300 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+              />
             </div>
             <div className="group relative">
-              <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                <a href={post.href}>
-                  <span className="absolute inset-0" />
-                  {post.title}
-                </a>
+              <h3 className="mt-3 text-lg font-semibold leading-6">
+                <input
+                  type="text"
+                  placeholder="Front-end Development"
+                  className="outline-none text-black "
+                />
               </h3>
-              <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
-                {post.description}
-              </p>
+              <input
+                className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600 outline-none"
+                placeholder="Add Hackathon topic details"
+              />
             </div>
           </article>
         ))}
