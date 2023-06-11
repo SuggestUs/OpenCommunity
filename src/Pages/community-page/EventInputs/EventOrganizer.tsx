5eef4ec89ce6b47e982548ddb4bfe59f575f6888
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2023-06-11 09:13:52 +0530

    feat: event page completed

---
+++ src/Pages/community-page/EventInputs/EventOrganizer.tsx
@@ -0,0 +1,42 @@
+export default function EventOrgCom() {
+  const posts = [
+    {
+      id: 1,
+      title: "Boost your conversion rate",
+      href: "#",
+      description:
+        "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+      date: "Mar 16, 2020",
+      datetime: "2020-03-16",
+    },
+  ];
+  return (
+    <div className="mx-auto text-left w-full pb-10 ">
+      <div className="mx-auto mt-10 grid w-full grid-cols-1 gap-x-8 gap-y-16  lg:mx-0 lg:max-w-none lg:grid-cols-3">
+        {posts.map((post) => (
+          <article
+            key={post.id}
+            className="flex w-[20rem] border p-5 hover:border-gray-600 rounded-md flex-col "
+          >
+            <div className="flex items-center gap-x-4 text-xs">
+              <time dateTime={post.datetime} className="text-gray-500">
+                {post.date}
+              </time>
+            </div>
+            <div className="group relative">
+              <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                <a href={post.href}>
+                  <span className="absolute inset-0" />
+                  {post.title}
+                </a>
+              </h3>
+              <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
+                {post.description}
+              </p>
+            </div>
+          </article>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ src/Pages/community-page/EventInputs/EventSpeakers.tsx
@@ -0,0 +1,41 @@
+export default function EventSpeakersCom() {
+  const people = [
+    {
+      name: "Leslie Alexander",
+      role: "Co-Founder / CEO",
+      imageUrl:
+        "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  ];
+  return (
+    <div className="mx-auto grid max-w-7xl gap-x-8  gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
+      <ul
+        role="list"
+        className=" gap-x-8 gap-y-12 sm:grid-cols-3 sm:gap-y-16 xl:col-span-2"
+      >
+        {people.map((person) => (
+          <li key={person.name}>
+            <div className="flex items-center gap-x-6">
+              <input
+                className="border rounded-md p-2  "
+                src={person.imageUrl}
+                type="file"
+                accept="image/*"
+              />
+              <div className="space-y-3">
+                <input
+                  className="text-base outline-none border p-2 rounded-md text-gray-900"
+                  placeholder="Speaker Name "
+                />
+                <input
+                  className="text-base outline-none p-2 border text-indigo-600 rounded-md"
+                  placeholder="Speaker's small bio"
+                />
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
